322eea23226a9faf1ce7408a4721af6281522c39
LanoCodes <del.pow1@gmail.com>
2022-11-25 10:52:59 -0500

    polished code and added in functionality to check what states were missed

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ turtle.shape(img)
 state_data = pd.read_csv("50_states.csv")
 
 all_states = []
+remaining_states_x = []
+remaining_states_y = []
+
 def need_to_learn():
     """Fills a list with all the states. Said list will be used to determine what states the user needs to learn"""
     for state in state_data["state"]:
         all_states.append(state)
-    print(all_states)
-need_to_learn()
 
 def check_for_state(user_guess):
     """Checks whether user's guess is a state or not"""
@@ -25,6 +26,14 @@ def check_for_state(user_guess):
         if user_guess == state:
             return True
     return False
+
+def fill_coords(x_list, y_list):
+    """Adds the necessary coordinates for the states not guessed into the 'x' or 'y' coord lists."""
+    for state in all_states:
+        row_for_state = state_data[state_data["state"] == state]
+        x_list.append(int(row_for_state["x"]))
+        y_list.append(int(row_for_state["y"]))
+    print(type(x_list[1]))
 
 test_turtle = turtle.Turtle()
 test_turtle.penup()
@@ -54,5 +63,14 @@ while total_states_guessed != 50:
     else:
         print("Not quite it")
 
-# states_to_learn.csv
-# I could have a list filled with all 50 states, but whenever the user correctly guesses a state, that states gets removed from the list
+fill_coords(remaining_states_x, remaining_states_y)
+
+all_states_dict = {
+    "State": all_states,
+    "x": remaining_states_x,
+    "y": remaining_states_y
+}
+
+remaining_states_df = pd.DataFrame.from_dict(all_states_dict)
+print(remaining_states_df)
+remaining_states_df.to_csv("states_to_learn.csv")
